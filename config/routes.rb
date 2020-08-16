a4b5f2140ccc2c8f17aef33e8dd40f5a086e1c1b
Larry Nevins <ichbinlarry@gmail.com>
2020-08-16 10:12:39 +0700

    add banner welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  root "welcome#index"
   get 'about/motto'
   get 'about/history'
   get 'about/awards'
   get 'welcome/index'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
